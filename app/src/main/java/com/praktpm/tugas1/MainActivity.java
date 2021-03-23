e31fa6c862bfb81dbc7740eac6f2eeeeb6623aea
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2021-03-23 17:14:06 +0700

    first commit

---
+++ app/src/main/java/com/praktpm/tugas1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.praktpm.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blog);
+    }
+}
